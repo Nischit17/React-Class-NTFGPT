49c17804ba2fa24e864ac2ed2840a9094b80b27c
unknown <nischit819@gmail.com>
2023-10-11 19:48:03 +0530

    episode-01-part-02-01(Sign In and Sign Up Form)

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="absolute px-96 py-1 bg-gradient-to-b from-black z-10">
+      <img
+        className="w-[155px]"
+        src="https://logohistory.net/wp-content/uploads/2023/05/Netflix-Logo.png"
+        alt="logo"
+      />
+    </div>
+  );
+};
+
+export default Header;
