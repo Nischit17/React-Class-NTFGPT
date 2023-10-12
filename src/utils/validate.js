1ab31c66ba578ac3f770a18af977c3752da4ca86
unknown <nischit819@gmail.com>
2023-10-12 19:14:48 +0530

    episode-01-part-3(Form Validation)

---
+++ src/utils/validate.js
@@ -0,0 +1,14 @@
+export const checkValidData = (email, password, name) => {
+  const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(
+    email
+  );
+  const isPasswordValid =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+
+  const isNameValid = /^[A-Za-z\s]{1,}[.]{0,1}[A-Za-z\s]{0,}$/.test(name);
+
+  if (!isEmailValid) return "Email ID is not Valid";
+  if (!isPasswordValid) return "Password is not Valid";
+  if (!isNameValid) return "Name is not Valid";
+  return null;
+};
